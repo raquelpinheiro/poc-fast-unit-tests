4760d055a11146a76b3f14c59aaf8713822e0da7
raquelpinheiro <raqueljpinheiro@gmail.com>
2021-12-16 21:50:27 -0300

    adiciona template projeto

---
+++ src/Poc.TrainingRunner.Domain/Entities/Training.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Poc.TrainingRunner.Domain.Entities
+{
+    public class Training
+    {
+        public TimeSpan DuringTime { get; set; }
+
+        public DayOfWeek Day { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
